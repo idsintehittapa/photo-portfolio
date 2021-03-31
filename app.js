7003c29173f11709215f83a06e255c1a79a520f4
idsintehittapa <jblom15@gmail.com>
2021-03-31 17:22:27 +0200

    added draft for JS slider

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/// this is to open the other portfolio projects
 const arrow = document.querySelector(".main__portfolio-links button")
 
 const arrowLinks = document.querySelector(".main__portfolio-links ul")
@@ -11,6 +12,52 @@ arrow.addEventListener('click', () => {
   })
 })
 
+
+// Carousel-slider Cinemagrah
+const carouselSlide = document.querySelector('.portfolio__carousel-slide')
+const carouselVideo = document.querySelectorAll('.portfolio__carousel-slide video')
+
+
+// Counter Cinemagrah
+let counter = 1
+const size = carouselVideo[0].clientWidth
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+
+// Buttons Carousel-slider Cinemagrah
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+// Button listeners 
+
+nextBtn.addEventListener('click', () => {
+  if (counter >= carouselVideo.length -1) return 
+  carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter++;
+  console.log(counter)
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+})
+
+prevBtn.addEventListener('click', () => {
+  if (counter <= 0) return
+  carouselSlide.style.transition = "transform 0.4s ease-in-out"
+  counter--
+  console.log(counter)
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+})
+
+carouselSlide.addEventListener('transitionend', () => {
+  if (carouselVideo[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none"
+    counter = carouselVideo.length - 2
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+  }
+  if (carouselVideo[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none"
+    counter = carouselVideo.length - counter
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+  }
+})
 
 // ////  JavaScript function that resizes an item so that it is just 
 // // large enough to display all the content within it.
