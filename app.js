92c7ddd27cdce94b6fd27ca0951eec0b459907f2
idsintehittapa <jblom15@gmail.com>
2021-04-06 17:14:51 +0200

    fixed width problem with carousel-slider

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 /// this is to open the other portfolio projects
 const arrow = document.querySelector(".main__portfolio-links button")
-
 const arrowLinks = document.querySelector(".main__portfolio-links ul")
-
 const links = document.querySelectorAll(".main__portfolio-links li")
 
 arrow.addEventListener('click', () => {
@@ -14,30 +12,32 @@ arrow.addEventListener('click', () => {
 
 
 // Carousel-slider Cinemagrah
+// Adding window.onload all the videos are loaded before the width is calculated
+// making the slides work!
+// https://stackoverflow.com/questions/57981312/clientwidth-width-in-javascript-is-not-working-on-img-tag
 
-// document.getElementById('vid').play(); 
+window.onload = () => {
 
 const carouselSlide = document.querySelector('.portfolio__carousel-slide')
 const carouselVideo = document.querySelectorAll('.portfolio__carousel-slide video')
-
 
 // Counter Cinemagrah
 let counter = 1
 const size = carouselVideo[0].clientWidth
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+console.log(size)
 
 // Buttons Carousel-slider Cinemagrah
 const prevBtn = document.querySelector('#prevBtn')
 const nextBtn = document.querySelector('#nextBtn')
 
 // Button listeners 
-
 nextBtn.addEventListener('click', () => {
   if (counter >= carouselVideo.length -1) return 
   carouselSlide.style.transition = "transform 0.4s ease-in-out"
     counter++;
-  console.log(counter)
+  console.log(counter, size)
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
 })
 
@@ -61,6 +61,7 @@ carouselSlide.addEventListener('transitionend', () => {
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
   }
 })
+}
 
 // ////  JavaScript function that resizes an item so that it is just 
 // // large enough to display all the content within it.
