77bc2173a9d8f6f1a59c73ab779dea9730e77036
idsintehittapa <jblom15@gmail.com>
2021-04-07 19:46:20 +0200

    added menu on mobile display on all pages

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+/// Nav menu
+const plus = document.querySelector('.site__nav-button')
+const navLinks = document.querySelector('.site__nav-list')
+const navLinksLi = document.querySelectorAll('.site__nav-link')
+
+plus.addEventListener('click', () => {
+  navLinks.classList.toggle('open')
+})
+
 /// this is to open the other portfolio projects
 const arrow = document.querySelector(".main__portfolio-links button")
 const arrowLinks = document.querySelector(".main__portfolio-links ul")
