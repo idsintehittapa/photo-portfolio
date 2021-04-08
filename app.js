11981b0e56f340e8fabcb1260e79878734f00b03
idsintehittapa <jblom15@gmail.com>
2021-04-08 20:56:15 +0200

    added teeth content and slider

--- app.js
+++ app.js
@@ -72,6 +72,32 @@ carouselSlide.addEventListener('transitionend', () => {
 })
 }
 
+// Teeth Slider
+const slider = document.querySelector('.slider')
+const before = document.querySelector('.teeth__img-before')
+const after = document.querySelector('.teeth__img-after')
+const container = document.querySelector('.teeth__container')
+
+const drag = (event) => {
+  let xPos = event.layerX
+  let sliderSize = container.offsetWidth
+  after.style.width = xPos + 'px'
+  slider.style.left = xPos + 'px'
+
+  if (xPos < 50) {
+    after.style.width = 0
+    slider.style.left = 0
+  }
+
+  if (xPos + 50 > sliderSize) {
+    after.style.width = sliderSize + 'px'
+    slider.style.left = sliderSize + 'px'
+  }
+}
+
+container.addEventListener('mousemove', drag)
+container.addEventListener('touchmove', drag)
+
 // ////  JavaScript function that resizes an item so that it is just 
 // // large enough to display all the content within it.
 // // This function resizes items individually
