a61adf6d7b705b97db1282516e0066f62eeb9344
idsintehittapa <jblom15@gmail.com>
2021-04-12 13:06:21 +0200

    added animation on bio

--- teeth.js
+++ teeth.js
@@ -1,24 +1,3 @@
-// /// Nav menu
-// const plus = document.querySelector('.site__nav-button')
-// const navLinks = document.querySelector('.site__nav-list')
-// const navLinksLi = document.querySelectorAll('.site__nav-link')
-
-// plus.addEventListener('click', () => {
-//   navLinks.classList.toggle('open')
-// })
-
-// /// this is to open the other portfolio projects
-// const arrow = document.querySelector(".main__portfolio-links button")
-// const arrowLinks = document.querySelector(".main__portfolio-links ul")
-// const links = document.querySelectorAll(".main__portfolio-links li")
-
-// arrow.addEventListener('click', () => {
-//   arrowLinks.classList.toggle('open')
-//   links.forEach(link => {
-//     link.classList.toggle('fade')
-//   })
-// })
-
 // Teeth Slider
 const slider = document.querySelector('.slider')
 const before = document.querySelector('.teeth__img-before')
